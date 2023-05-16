66a21e2eec3e5017ef44c8117b34b0f280ddc7f0
Nplace-su <105898543+Nplace-su@users.noreply.github.com>
2023-05-16 16:11:32 +0800

    Fix wrong subscription start time when there already is a same plan. (#3)
    * handle plan time
    
    * Update route.ts
    
    * Update route.ts
    
    * Update route.ts
    
    * Update route.ts
    
    * Update route.ts

--- apps/chat/src/app/api/callback/route.ts
+++ apps/chat/src/app/api/callback/route.ts
@@ -2,6 +2,18 @@ import { NextRequest } from "next/server";
 import { handleCallback } from "@/lib/pay/xunhu";
 import { OrderLogic, SubscriptionLogic } from "database";
 
+
+const getLastMatchingSubscriptionEndsAt = (subscriptions: { plan: string; startsAt: number; endsAt: number; tradeOrderId: string; }[] | null, orderPlan: string): number => {
+  if (subscriptions === null) {
+    return Date.now();
+  }
+  for (let i = subscriptions.length - 1; i >= 0; i--) {
+    if (subscriptions[i].plan === orderPlan) {
+      return subscriptions[i].endsAt;
+    }
+  }
+  return Date.now();
+};
 /**
  * This is the callback interface for processing payment platforms.
  * @constructor
@@ -23,9 +35,11 @@ export async function POST(req: NextRequest) {
 
   // Add subscription for users.
   const subscriptionLogic = new SubscriptionLogic();
+  const userSubscription = await subscriptionLogic.listUserSubscriptions(order!.email);
+  const startsAt = getLastMatchingSubscriptionEndsAt(userSubscription, order!.plan);
   await subscriptionLogic.append(order!.email, {
-    startsAt: Date.now(),
-    endsAt: Date.now() + 1000 * 60 * 60 * 24 * 31 * order!.count,
+    startsAt,
+    endsAt: startsAt + 1000 * 60 * 60 * 24 * 31 * order!.count,
     plan: order!.plan,
     tradeOrderId: orderId,
   });
