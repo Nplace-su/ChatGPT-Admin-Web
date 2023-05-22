ab7134b642fb3669c4fbe2a018285164904e0c8f
Nplace-su <105898543+Nplace-su@users.noreply.github.com>
2023-05-22 18:28:36 +0800

    Update items.tsx

--- apps/dash/src/app/[area]/items.tsx
+++ apps/dash/src/app/[area]/items.tsx
@@ -60,6 +60,10 @@ export const userColumn: TableColumnType[] = [
     label: "用户名",
   },
   {
+    prop: "user",
+    label: "邮箱",
+  },
+  {
     prop: "subscriptions",
     label: "订阅",
     render: planRender,
@@ -103,7 +107,7 @@ export const orderColumns = [
   {
     prop: "plan",
     label: "计划",
-    render: (plan: string) => getPlanTag(plan),
+    // render: (plan: string) => getPlanTag(plan),
   },
   {
     prop: "status",
